d1a4766590cfc31aa30df9a678915e6ac4c9a27b
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2019-10-28 12:42:45 -0300

    ibagens 400x300

---
+++ URI/teste.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+typedef unsigned int uint;
+typedef unsigned long long int ulli;
+
+//typedef enum Bool {
+//	true = 1, false = 0
+//} bool;
+
+int main(void){
+    int x = 10;
+    int y = 20;
+    while (x ++< y)
+    {
+        printf("%d\n", x);
+    }
+    
+    return 0;
+}
